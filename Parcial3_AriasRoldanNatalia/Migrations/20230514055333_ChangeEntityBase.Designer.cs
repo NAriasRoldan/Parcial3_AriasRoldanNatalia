b30e1132b269f1ade5a02a0459d092463046027b
Natalia Arias <nataliaarias523@gmail.com>
2023-05-14 07:25:22 -0500

    Modify Date for views

--- Parcial3_AriasRoldanNatalia/Migrations/20230514055333_ChangeEntityBase.Designer.cs
+++ Parcial3_AriasRoldanNatalia/Migrations/20230514055333_ChangeEntityBase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parcial3_AriasRoldanNatalia.DAL;
 
@@ -11,9 +12,11 @@ using Parcial3_AriasRoldanNatalia.DAL;
 namespace Parcial3_AriasRoldanNatalia.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230514055333_ChangeEntityBase")]
+    partial class ChangeEntityBase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Parcial3_AriasRoldanNatalia/Migrations/20230514055333_ChangeEntityBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial3_AriasRoldanNatalia.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEntityBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicules_Servicies_ServicesId",
+                table: "Vehicules");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ServicesId",
+                table: "Vehicules",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicules_Servicies_ServicesId",
+                table: "Vehicules",
+                column: "ServicesId",
+                principalTable: "Servicies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicules_Servicies_ServicesId",
+                table: "Vehicules");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ServicesId",
+                table: "Vehicules",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicules_Servicies_ServicesId",
+                table: "Vehicules",
+                column: "ServicesId",
+                principalTable: "Servicies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
